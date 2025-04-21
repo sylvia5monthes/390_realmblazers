bcae5c8cfa1092beb16533b209283962c52c40c4
yao-lyndrinn <yao.lyndrinn@gmail.com>
2025-04-21 05:39:49 -0400

    spark fix

--- Assets/Scripts/Units/ImpEnemyUnit.cs
+++ Assets/Scripts/Units/ImpEnemyUnit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ImpEnemyUnit : Unit
 {   
-    float[] spark = new float[] {0, 7, 0.9f, 2}; //index 1: physical vs magic. 1 is magic, 0 is phys. then: power, accuracy, and range
+    float[] spark = new float[] {1, 7, 0.9f, 2}; //index 1: physical vs magic. 1 is magic, 0 is phys. then: power, accuracy, and range
     string action0 = "Spark";
     // Start is called before the first frame update
     protected override void Start()
