5299c060ce4c8f4059984c133a93799216038ac9
yao-lyndrinn <yao.lyndrinn@gmail.com>
2025-04-22 04:51:41 -0400

    adding prefabs into vfx

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -102,7 +102,11 @@ public class EnemyController : MonoBehaviour
             if (unit.currentHealth > 0)
             {
                 unit.EnemyLogic();
-                yield return new WaitForSeconds(3.0f);
+                if (unit.unitDisplayName != "Crystal"){
+                    yield return new WaitForSeconds(2.5f);
+                } else{
+                    yield return new WaitForSeconds(1.5f);
+                }
             }
         }
         yield return new WaitForSeconds(2.0f);
