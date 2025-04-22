109da2f8032b48c12401f4be1218c27ccd336108
Ashton Caldwell <ashton.caldwell@duke.edu>
2025-04-21 20:36:50 -0400

    Created scripts necessary for dialogue.

---
+++ Assets/Scripts/Dialogue/Line.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Line
+{
+    
+    public string speaker;
+    [TextArea(2, 5)] public string text;
+    public Sprite portrait;
+
+}
---
+++ Assets/Scripts/Dialogue/Sequence.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Dialogue/Sequence")]
+public class Sequence : ScriptableObject
+{
+    
+    public List<Line> lines;
+
+}
