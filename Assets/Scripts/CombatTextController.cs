31361c0c63e25592978285762e9ef9b43805b015
Connie Xu <cx71@duke.edu>
2025-04-22 00:18:35 -0400

    fix commit

--- Assets/Scripts/CombatTextController.cs
+++ Assets/Scripts/CombatTextController.cs
@@ -73,7 +73,7 @@ public class CombatTextController : MonoBehaviour
         if (damage > 0){
             receiverText.text = receiverName + displayString1 + damage.ToString() + displayString2 + actorName + "!";
             receiverText.gameObject.SetActive(true);
-            yield return new WaitForSeconds(2.0f);
+            yield return new WaitForSeconds(3.0f);
             receiverText.gameObject.SetActive(false);
             combatTextPanel.SetActive(false);
         }
@@ -83,7 +83,7 @@ public class CombatTextController : MonoBehaviour
     }
     private IEnumerator HideAfterDelay()
     {
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
         actorText.gameObject.SetActive(false);
         combatTextPanel.SetActive(false);
     }
